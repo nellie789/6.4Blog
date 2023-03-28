f96487b43962030d382c7fd7c0aafd1b90572ee8
nellie789 <62j9@fresnou.org>
2023-03-28 11:34:25 -0700

    adding article

---
+++ controllers/articleController.js
@@ -0,0 +1,56 @@
+const {Article} = require('../models');
+
+module.exports.renderAddForm = function(req, res){
+    const article = {
+        title: '',
+        intro: '',
+        image_url: '',
+        body: ''
+    };
+    res.render('articles/add', {article});
+};
+
+module.exports.addArticle = async function(req, res){
+    const article = await Article.create({
+        title: req.body.title,
+        intro: req.body.intro,
+        image_url: req.body.image_url,
+        body: req.body.body,
+        author_id: 1, //todo get logged in user
+        published_on: new Date()
+    });
+    res.redirect('/') //todo change the redirect to view all once made
+};
+
+module.exports.displayArticle = async function(req, res){
+    const article = await Article.findByPk(req.params.articleId,{
+        include: ['author']
+    });
+    res.render('articles/view', {article});
+};
+
+module.exports.displayAll = async function(req, res){
+    const articles = await Article.findAll({
+        include: ['author']
+    });
+    res.render('articles/viewAll', {articles});
+};
+
+module.exports.renderEditForm = async function(req, res){
+    const article = await Article.findByPk(req.params.articleId);
+    res.render('articles/edit', {article});
+};
+
+module.exports.updateArticle = async function(req, res){
+    await Article.update({
+        title: req.body.title,
+        intro: req.body.intro,
+        image_url: req.body.image_url,
+        body: req.body.body,
+    }, {
+        where: {
+            id: req.params.articleId
+        }
+    });
+    res.render(`/article/${req.params.articleId}`);
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+const articleController = require('../controllers/articleController');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next){
+  res.redirect('/article');
 });
+
+router.get('/article/add', articleController.renderAddForm);
+router.post('/article/add', articleController.addArticle);
+
+router.get('/article/:articleId', articleController.displayArticle);
+router.get('/article/', articleController.displayAll);
+router.get('/article/:articleId/edit', articleController.renderEditForm);
+router.post('/article/:articleId/edit', articleController.updateArticle);
 
 module.exports = router;
